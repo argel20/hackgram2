88a10904bd4aed28e05781385018b08e172884c5
abiasco3047 <argelbiascoayala20@gmail.com>
2016-06-28 11:23:00 -0430

    comments listos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,15 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
     redirect_to post_path(@post)
+    @comment.save!
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit([:content])
+    params.require(:comment).permit([:user_id,:content,:post_id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content,:avatar, :type_of,comments_attributes: [:id, :body])
+    params.require(:post).permit(:title, :content,:avatar, :type_of, :user_id)
   end
 
 end
