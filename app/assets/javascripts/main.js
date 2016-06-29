384b828b2b93201c39e5374c5f8079399352eee5
abiasco3047 <argelbiascoayala20@gmail.com>
2016-06-28 19:40:24 -0430

    casi listo el ajax

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,27 +1,13 @@
-// $("#buton").click(function(){
-//   $.ajax("/posts",{
-//   success: function(data){
-//
-//     console.log(data);
-//   }
-// });
-// });
-    // $.each(response, function(index, user){
-    //   $('#'+index+'').append('<td >'+User.name+'</td>')
-    //
-    // $('tbody').append('<tr id='+index+'></tr>')
-    //
-    // });
-    // }
 $('#form_post').submit(function(e){
   e.preventDefault();
+  $.ajax("/posts",{
+    dataType: 'json',
+    type: 'POST',
+    data:$('#form_post').serialize(),
+    success: function(data) {
+    console.log(data);
+    $('#form_post').append('<div class="row "><div class="col-md-12"><div class="thumbnail"><div class="thumbnail"><div class="media"><div class="media-left media-middle"><a href="#"></a><img alt="..." class="media-object" src="assets/user3-100x100.jpg" style="width:64px; height:64px"/></div><div class="media-body"><h4 class="media-heading">'+data.content+'</h4></div></div></div><div class="iteracion"><a href="posts/<%= post.id %>"><span class="glyphicon glyphicon-comment"></span></a></div><div class="iteracion"><span class="glyphicon glyphicon-share"></span></div><div class="iteracion"><span class="glyphicon glyphicon-heart"></span></div></div></div></div>')
+  },
 
-  console.log($('#form_post'));
-  location.reload()
-  // $('.argel').load('/posts',.row.argel)
-    // $('#form_post').append('<div class="thumbnail"><div class="thumbnail"><div class="media"><div class="media-left media-middle"><a href="#"></a><img alt="..." class="media-object" src="assets/user3-100x100.jpg" style="width:64px; height:64px"/></div><div class="media-body"><h4 class="media-heading">"pureba"</h4></div></div></div></div>')
-
+  });
 });
-
-// <div class="thumbnail"><div class="thumbnail"><div class="media"><div class="media-left media-middle"><a href="#"></a><img alt="..." class="media-object" src="assets/user3-100x100.jpg" style="width:64px; height:64px"/></div><div class="media-body"><h4 class="media-heading">prueba</h4></div></div></div></div>
-//
